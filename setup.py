c2a94ed66455e607165bd77d855e9280e8200bde
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-27 20:54:44 -0300

    added setup file and version file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+execfile('magicalbum/version.py')
+
+setup(
+    name='django-magic-album',
+    version=__version__,
+    description="Django Magic Album is an album that picks picutes from " +
+    "Twitter, sends emails and posts to Facebook",
+    long_description=open('README.md').read(),
+    author="Ivan Neto",
+    author_email="espen@hogbakk.no",
+    url="http://github.com/ivancrneto/django-magic-album",
+    packages=['magicalbum'],
+    include_package_data=True,
+)
