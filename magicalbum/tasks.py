91f7d6bfb6d901331789c425072c7fdbed54d635
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-28 05:59:07 -0300

    putting only task in magicalbum task file

--- magicalbum/tasks.py
+++ magicalbum/tasks.py
@@ -1,15 +1,8 @@
-from celery import Celery
-from celery.schedules import crontab
-import os
+from celery import shared_task
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
 
-app = Celery('magicalbum', broker=os.environ['MAGICALBUM_TASKS_BROKER'])
-app.conf.CELERYBEAT_SCHEDULE = {
-    'poll-twitter-photos-every-twenty-minutes': {
-        'task': 'tasks.get_twitter_photos',
-        'schedule': crontab(),
-    },
-}
 
-@app.task
+@shared_task
 def get_twitter_photos():
     return True
