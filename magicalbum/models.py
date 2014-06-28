3ca6f8df202c3da4dc6f0dd005bd941f6ce04dcd
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-28 06:30:33 -0300

    added get last sinceid function to tweetscontrol model

--- magicalbum/models.py
+++ magicalbum/models.py
@@ -8,4 +8,9 @@ class Album(models.Model):
 
 class TweetsControl(models.Model):
     since_id = models.CharField(max_length=100)
-    created_on = models.DatetimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def get_last_since_id(self):
+        if not self.objects.count():
+            return 0
+        return int(self.objects.order_by('-created_on')[0].since_id)
