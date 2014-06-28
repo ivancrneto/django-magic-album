6cfe6fcc5421b457fcdbbf297944f999e491fdeb
Ivan Neto <ivan.cr.neto@gmail.com>
2014-06-28 15:35:41 -0300

    added first test for album view

--- magicalbum/tests.py
+++ magicalbum/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+""" Module for Magic Album tests """
 
-# Create your tests here.
+from django.test import TestCase
+from django.core.urlresolvers import reverse as r
+
+
+class TestAlbumView(TestCase):
+    """ Class for testing Magic Album view """
+
+    def test_status_code(self):
+        """ Response status code for album view should be 200 """
+        resp = self.client.get(r('magicalbum:album'))
+        self.assertEqual(200, resp.status_code)
